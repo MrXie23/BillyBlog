a817189e6f997d128254509bd0166d110e4ce120
BillyXie23 <330764937@qq.com>
2025-04-17 18:08:09 +0800

    回滚 next.config.js 到 update yarn 提交状态

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-import { withContentlayer } from 'next-contentlayer2'
-import bundleAnalyzer from '@next/bundle-analyzer'
+const { withContentlayer } = require('next-contentlayer2')
 
-const withBundleAnalyzer = bundleAnalyzer({
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.billyxie.com giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
