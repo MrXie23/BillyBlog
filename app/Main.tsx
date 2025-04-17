061624bc1ae339606c00c6337b934e3898182194
BillyXie23 <330764937@qq.com>
2025-04-17 17:45:25 +0800

    移除react-chat-widget

--- app/Main.tsx
+++ app/Main.tsx
@@ -127,7 +127,7 @@ export default function Home({ posts }) {
           <NewsletterForm />
         </div>
       )} */}
-      <SimpleChatWidget />
+      {/* <SimpleChatWidget /> */}
     </>
   )
 }
